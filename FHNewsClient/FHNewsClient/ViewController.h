a2d1a2d9cb2c063fec0e37412af7fa4c362ec0ff
@your name <your@email.com>
2016-08-22 15:34:50 +0800

    仿写凤凰新闻客户端

---
+++ FHNewsClient/FHNewsClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FHNewsClient
+//
+//  Created by mac on 16/8/22.
+//  Copyright © 2016年 xiayuanquan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FHNewsClient/FHNewsClient/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FHNewsClient
+//
+//  Created by mac on 16/8/22.
+//  Copyright © 2016年 xiayuanquan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
